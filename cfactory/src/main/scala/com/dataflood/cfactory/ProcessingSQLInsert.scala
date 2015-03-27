20ba2e43080ea3ca8827a68eec8c695fc3977ef0
v-dchekh <rrrttt222@tut.by>
2015-03-27 09:13:28 -0300

    added SQL insert

--- cfactory/src/main/scala/com/dataflood/cfactory/ProcessingSQLInsert.scala
+++ cfactory/src/main/scala/com/dataflood/cfactory/ProcessingSQLInsert.scala
@@ -3,16 +3,22 @@ package com.dataflood.cfactory
 import scala.collection.mutable.ArrayBuffer
 import org.apache.avro.generic.{ GenericRecord }
 import scala.collection.mutable.{ ListMap, HashMap }
+import java.sql.Statement
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.Connection
 
 class ProcessingSQLInsert extends Processing {
 
-  def run(messageArray: ArrayBuffer[GenericRecord]) {
+  def run(messageArray: ArrayBuffer[GenericRecord], threadId: Int) {
     val count_ = messageArray.size
     val toSQL = ArrayBuffer[Map[String, String]]()
     val toSQLAny = ArrayBuffer[Any]()
+    val toSQLValues = ArrayBuffer[String]()
+    var schemaDoc : String = ""
     messageArray.foreach { x =>
       var schemaFields = x.getSchema.getFields
-      var schemaDoc = x.getSchema.getDoc
+      schemaDoc = x.getSchema.getDoc
       //var a = 0
       var recordToMap = new HashMap[String, Any]
       for (a <- 0 until schemaFields.size()) {
@@ -22,7 +28,7 @@ class ProcessingSQLInsert extends Processing {
         val fieldType_ = field.schema().getType
         val fieldType2_ = field.schema().getType.getName
         val fieldValue = x.get(a).toString()
-        val fieldValueSQL = fieldType_.toString()  match {
+        val fieldValueSQL = fieldType_.toString() match {
           case "STRING" => {
             if (fieldValue == "%null%") "null"
             else "'" + fieldValue.replace("'", "'''") + "'"
@@ -48,11 +54,29 @@ class ProcessingSQLInsert extends Processing {
       val sqlValues = recordToMapSorted2.toMap.values.toList.mkString("(", ",", ")")
       val sqlStr = sqlFields + sqlValues
       toSQL += Map(sqlFields -> sqlValues)
-      val l = (sqlFields, sqlValues, schemaDoc)
+      val l = (schemaDoc, sqlFields, sqlValues)
       toSQLAny += l
+      toSQLValues += "%1% " + sqlValues
     }
     logger.debug("-----------------------------------------------------------------------------------------")
-    logger.debug(toSQLAny.toList.mkString("\n").replace("),", ")|"))
+    //    logger.debug(toSQLAny.toList.mkString("\n").replace("),", ")|"))
+    //    logger.debug(toSQLAny.toList.mkString("\n").replace("),(", ") values (").toString())
+    logger.debug(toSQLValues.toList.mkString("\n"))
+    try {
+      var connection: Connection = CFactory.arrayConnection(threadId)
+      var pstmt = connection.prepareCall("{? = call dbo.ConsumerMSSQL(?,?)}")
+      pstmt.registerOutParameter(1, java.sql.Types.INTEGER);
+      pstmt.setString(2, "1" ) 
+      pstmt.setString(3, toSQLValues.toList.mkString("\n")) 
+      pstmt.execute()
+      val resultSet = "RETURN STATUS: " + pstmt.getInt(1)
+      logger.debug(resultSet)
+      
+    } catch {
+      case e: Throwable =>
+        if (true) logger.info(e) 
+        else throw e
+    }
 
   }
 
